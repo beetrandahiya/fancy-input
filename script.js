7776879af3be3d125cbcc03a86f90c362495d134
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-07-25 00:38:48 +0530

    done deletion

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function anim() {
     //get cursor position
     pos = inp.selectionStart;
     // if length increases add the letter to div
+    inp.addEventListener("click", function() {
+        //get the selection start and end
+        start = inp.selectionStart;
+        end = inp.selectionEnd;
+        //get the selection
+        selection = inp.value.substring(start, end);
+        //get the selection length
+        selectionlength = selection.length;
+        });
 
     if (val.length > oldval.length) {
         inputletter = val[val.length - 1];
@@ -21,16 +30,23 @@ function anim() {
         //add an animation to the span    
 
     }
+ 
     // if length decreases remove the letter from div
     else if (val.length <= oldval.length) {
-        //get how many letters are deleted
-        deleted = oldval.length - val.length + 1;
-        //remove all the deleted letters from the div
-        for (i = 0; i < deleted; i++) {
-            //todo : add an animation to the removed letter
-            document.getElementById("inp_div").removeChild(document.getElementById("inp_div").lastChild);
+        //get the selectionstart and selectionend
+        if(typeof start !== 'undefined' && start!=end){
+        console.log(start, end);
+        //remove the letter from the div
+        for(i = start; i <end; i++){
+            document.getElementById("inp_div").removeChild(document.getElementById("inp_div").childNodes[start]);
+            }
         }
-
+        else{
+            //get cursor position
+            pos = inp.selectionStart;
+            //remove the letter from the div
+            document.getElementById("inp_div").removeChild(document.getElementById("inp_div").childNodes[pos]);
+        }
     }
     
 
@@ -38,3 +54,4 @@ function anim() {
 
     oldval = val.substring(0, val.length - 1);
 }
+

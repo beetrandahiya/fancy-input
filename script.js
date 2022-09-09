b15913107ffc38a55c85b8deefa3951bcf051dcf
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-09-09 20:54:59 +0530

    added the best possible solution for styles

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // mutliple event listeners function
 const addMultipleListeners = (el, types, listener, options, useCapture) => {
     types.forEach(type =>
-      el.addEventListener(type, listener, options, useCapture)
+        el.addEventListener(type, listener, options, useCapture)
     );
-  };
-  
+};
+
 /* create a custom element */
 var FancyInput = customElements.define('fancy-input', class extends HTMLElement {
     constructor() {
@@ -17,32 +17,112 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
             mode: 'open'
         });
         //set default attributes
-        this.styles=this.formatStyles(this.getAttribute('styles'));
+        this.styles = this.formatStyles(this.getAttribute('styles'));
         this.shadowRoot.innerHTML += `
         <style>
         :host > input::selection {
             background: #44f8;
             color: transparent;
+        }        
+        @keyframes scale {
+            0% {
+                transform: scale(0);
+            }
+            50% {
+                transform: scale(1.1);
+            }
+            100% {
+                transform: scale(1);
+            }
+        } 
+        @keyframes drop {
+            0% {
+                transform: translateY(-30px);
+            }
+            80% {
+                transform: translateY(10px);
+            }
+            85% {
+                transform: translateY(-5px);
+            }
+            90% {
+                transform: translateY(10px);
+            }
+            100% {
+                transform: translateY(0px);
+            }
         }
-             
-@keyframes drop {
-    0% {
-        transform: translateY(-30px);
-    }
-    80% {
-        transform: translateY(10px);
-    }
-    85% {
-        transform: translateY(-5px);
-    }
-    90% {
-        transform: translateY(10px);
-    }
-    100% {
-        transform: translateY(0px);
-    }
-}
-</style>
+        @keyframes shake {
+            0% {
+                transform: translate(1px,2px) rotate(35deg);
+            }
+            20% {
+                transform: translate(-2px,1px) rotate(-35deg);
+            }
+            40% {
+                transform: translate(4px,-2px) rotate(35deg);
+            }
+            60% {
+                transform: translate(-5px,2px) rotate(-15deg);
+            }
+            100% {
+                transform: translate(0px);
+            }
+        }
+
+        @keyframes color-bloom{
+            0%{
+                color: hsl(0,100%,50%);
+                /*glow text*/
+                text-shadow: 0 0 10px hsl(0,100%,50%);
+
+            }
+            20%{
+                color: hsl(50,100%,50%);
+                text-shadow: 0 0 10px hsl(50,100%,50%);
+
+            }
+            40%{
+                color: hsl(100,100%,50%);
+                text-shadow: 0 0 10px hsl(100,100%,50%);
+
+            }
+            60%{
+                color: hsl(150,100%,50%);
+                text-shadow: 0 0 10px hsl(150,100%,50%);
+            }
+            80%{
+                color: hsl(200,100%,50%);
+                text-shadow: 0 0 10px hsl(200,100%,50%);
+
+            }
+            100%{
+                color: rgb(2, 1, 9);
+                text-shadow: 0 0 0px rgb(2, 1, 9);
+            }
+        }
+
+        @keyframes zoom {
+            0% {
+                transform: scale(1);
+            }
+            50% {
+                transform: scale(2);
+            }
+            100% {
+                transform: scale(1);
+            }
+        }
+
+        @keyframes fade-in {
+            0% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+        </style>
         `;
 
         this.inp = document.createElement('input');
@@ -79,9 +159,9 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
         this.inp.style.fontFamily = this.getAttribute('font-family') || this.styles['font-family'] || "sans-serif";
         this.inp.style.fontSize = this.getAttribute('font-size') || this.styles['font-size'] || "16px";
         this.inp.style.fontWeight = this.getAttribute('font-weight') || this.styles['font-weight'] || "normal";
-        this.inp.style.font= this.getAttribute('font') || this.styles.font || "inherit";
+        this.inp.style.font = this.getAttribute('font') || this.styles.font || "inherit";
         this.inp.style.color = "transparent";
-        this.inp.style.caretColor = this.getAttribute('caret-color') || this.styles['caret-color'] || "#000";        
+        this.inp.style.caretColor = this.getAttribute('caret-color') || this.styles['caret-color'] || "#000";
 
         // set styles for input div
         this.inp_div.style.position = "absolute";
@@ -94,7 +174,7 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
         this.inp_div.style.fontFamily = this.getAttribute('font-family') || this.styles['font-family'] || "sans-serif";
         this.inp_div.style.fontSize = this.getAttribute('font-size') || this.styles['font-size'] || "16px";
         this.inp_div.style.fontWeight = this.getAttribute('font-weight') || this.styles['font-weight'] || "normal";
-        this.inp_div.style.font= this.getAttribute('font') || this.styles.font || "inherit";
+        this.inp_div.style.font = this.getAttribute('font') || this.styles.font || "inherit";
         this.inp_div.style.color = this.getAttribute('color') || this.styles.color || "black";
         this.inp_div.style.border = this.getAttribute('border') || this.styles.border || "none";
         this.inp_div.style.borderRadius = this.getAttribute('border-radius') || this.styles['border-radius'] || "0px";
@@ -106,7 +186,7 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
 
         this.shadowRoot.appendChild(this.inp_div);
         this.shadowRoot.appendChild(this.inp);
-     
+
 
         //add event listener
         this.inp.addEventListener('input', this.anim_fn.bind(this));
@@ -114,14 +194,14 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
         //this.inp.oninput = this.anim_fn(this.inp);
 
         // make span elements for default value
-        if(this.getAttribute('value') != null){
-            for(var i=0; i<this.getAttribute('value').length; i++){
+        if (this.getAttribute('value') != null) {
+            for (var i = 0; i < this.getAttribute('value').length; i++) {
                 var span = document.createElement('span');
                 span.style.display = "inline-block";
                 span.style.lineHeight = this.getAttribute('height') || this.styles.height || "100%";
                 span.style.animation = this.getAttribute('animation') + " linear" + " forwards";
                 span.textContent = this.getAttribute('value')[i];
-                if(this.getAttribute('value')[i] == ' '){
+                if (this.getAttribute('value')[i] == ' ') {
                     span.style.display = 'initial';
                 }
                 this.inp_div.appendChild(span);
@@ -144,12 +224,13 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
         for (var i = 0; i < styles.length; i++) {
             var s = styles[i].trim();
             if (s != '') {
-            var s = styles[i].split(':');
-            style[s[0].trim()] = s[1].trim();}
+                var s = styles[i].split(':');
+                style[s[0].trim()] = s[1].trim();
+            }
         }
         console.log(style);
         return style;
-    }        
+    }
 
     anim_fn() {
         // get div next to the input
@@ -162,10 +243,12 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
 
         addMultipleListeners(
             elem,
-            ['keydown', 'selection', 'keyup','click'],
-            () => { this.getSelection() }
-          );
-        
+            ['keydown', 'selection', 'keyup', 'click'],
+            () => {
+                this.getSelection()
+            }
+        );
+
 
         if (this.val.length > this.oldval.length) {
             var pos = elem.selectionStart;
@@ -174,7 +257,7 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
             var span = document.createElement("span");
             span.style.display = "inline-block";
             span.style.lineHeight = this.getAttribute('height') || this.styles.height || "100%";
-            span.style.animation = this.getAttribute('animation')+ " linear"+" forwards";
+            span.style.animation = this.getAttribute('animation') + " linear" + " forwards";
             span.innerHTML = inputletter;
             if (inputletter == " ") {
                 span.style.display = "initial";
@@ -188,8 +271,8 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
         // if length decreases remove the letter from div
         else if (this.val.length <= this.oldval.length) {
             //get the selectionstart and selectionend
-        
-           if (typeof this.start != 'undefined' && this.start!=this.end){
+
+            if (typeof this.start != 'undefined' && this.start != this.end) {
                 //remove the letter from the div
                 for (var i = this.start; i < this.end; i++) {
                     this.inp_div.removeChild(this.inp_div.childNodes[this.start]);
@@ -199,11 +282,11 @@ var FancyInput = customElements.define('fancy-input', class extends HTMLElement 
                 var pos = elem.selectionStart;
                 //remove the letter from the div
                 this.inp_div.removeChild(this.inp_div.childNodes[pos]);
-            } 
-           
+            }
+
         }
 
-        if(this.oldval!=this.val){
+        if (this.oldval != this.val) {
             this.oldval = this.val.substring(0, this.val.length - 1);
         }
 
